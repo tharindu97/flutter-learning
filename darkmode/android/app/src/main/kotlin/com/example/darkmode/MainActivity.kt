e5ac98e3c7957d7971d711b7c861c350f1781855
Tharindu97 <tharindukavishna@gmail.com>
2020-09-14 18:13:01 +0530

    dark_mode

---
+++ darkmode/android/app/src/main/kotlin/com/example/darkmode/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.darkmode
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ firebasecloud/android/app/src/main/kotlin/com/example/firebasecloud/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.firebasecloud
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
