83036a1c804d2eb21358aebef0f232f039462ffb
Tharindu97 <tharindukavishna@gmail.com>
2020-10-06 07:46:14 +0530

    scill

---
+++ autoscalling/android/app/src/main/kotlin/com/example/autoscalling/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.autoscalling
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ autoscalling/lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  double _height;
+  double _initHeight;
+  
+  @override
+  void initState() {
+    _initHeight = 20.0;
+    _height = _initHeight;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: SizedBox(
+          height: _height,
+          child: FittedBox(
+            child: Text('data'),
+          ),
+        ),
+      ),
+    );
+  }
+}
